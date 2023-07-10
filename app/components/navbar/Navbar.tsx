2a69c2030e7e2adab305628900331e90e1ed942c
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-07-10 12:04:37 +0600

    project initialised and developing navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import Container from '../Container';
+
+
+
+
+const Navbar = () => {
+  return (
+    <div className='bg-white fixed w-full shadow-sm z-10'>
+    <div className='py-4 border-b-[1px]'>
+        <Container>
+
+        </Container>
+
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
